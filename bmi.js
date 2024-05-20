87c3b5b93be03da5ea4b6a156e9eda0087cb1618
MikeD3V <kakamike185@gmail.com>
2024-05-20 15:42:52 +0800

    Add files via upload

---
+++ bmi.js
@@ -0,0 +1,30 @@
+let weight = document.getElementById('weight');
+let height = document.getElementById('height');
+let result = document.getElementById('result');
+let calculate = document.getElementById('calculate');
+let peyc = document.getElementById('peyc');
+
+
+
+calculate.onclick = function(){
+    if(weight.value === "" || height.value === ""){
+        alert('Please fill the weight and height')
+    }
+    else{
+    let weightValue = Number(weight.value);
+    let heightValue = Number(height.value);
+    let r =(weightValue / (heightValue / 100) ** 2) ;
+    result.textContent = r;
+    if (r <= 18.5) {
+        peyc.src = '/javaPics/u.png';
+    } else if (r <= 24.9) {
+        peyc.src = '/javaPics/n.png';
+    } else if (r <= 29.9) {
+        peyc.src = '/javaPics/o.png';
+    } else {
+        peyc.src = '/javaPics/ob.png';
+    }
+}
+}
+    
+
